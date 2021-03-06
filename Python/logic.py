e8cfb2a49a36f26811f50e13ca92b125d7ea4f1b
mcmido <mcmido93@gmail.com>
2021-03-06 09:46:00 +0200

    login.py

---
+++ Python/logic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+name = "dave"
+age = 30
+
+name == "dave"
+
+
+
+age == 30 and name == "dave"
+
+
+(8+1 == 9 ) and (8 == 8 )
+
+
+# Booleans -- the thing we're actually dealing with
+
+is_authenticated = True
+is_admin = False
