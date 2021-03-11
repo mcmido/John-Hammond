820ed8c722c492f4ecfd7865551046f3386f04fd
mcmido <mcmido93@gmail.com>
2021-03-12 00:30:22 +0200

    Basic Login html + some backend

--- Python/tut-projects/Website-Login/website/__init__.py
+++ Python/tut-projects/Website-Login/website/__init__.py
@@ -2,4 +2,11 @@ from flask import Flask
 def creat_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "root"
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
