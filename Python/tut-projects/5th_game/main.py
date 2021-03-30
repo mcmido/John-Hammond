09bb8a8d4e070edfaa34859c903af12a148b08f5
Mohamed.F.Ebrahim <mcmido93@gmail.com>
2021-03-30 16:19:47 +0200

    Update main.py

--- Python/tut-projects/5th_game/main.py
+++ Python/tut-projects/5th_game/main.py
@@ -1,10 +1,25 @@
 import pygame, sys
-pygame.init()
+from pygame.math import Vector2
+class FRUIT:
+    def __init__(self):
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x,self.y)
 
+
+    def draw_fruit(self):
+        # create a rectangle
+        # draw the rectangle
+
+
+
+
+pygame.init()
+cell_size = 40
+cell_number = 20
 width = 500
 height = 600
-
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * ce ll_size))
 pygame.display.set_caption('SNAKE')
 bg = (234, 218, 184)
 
