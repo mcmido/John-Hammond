e3db08eb6cc27b85a476d424d06a7bcbc917029c
Mohamed.F.Ebrahim <mcmido93@gmail.com>
2021-04-03 17:02:27 +0200

    Update server.py

--- Python/tut-projects/online-game/server.py
+++ Python/tut-projects/online-game/server.py
@@ -1,3 +1,16 @@
 import socket
 import _thread
 import sys
+
+server = ""
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection,server started")
