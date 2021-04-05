950c548f9331d7014891d8b6185e63b6112f708d
Mohamed.F.Ebrahim <mcmido93@gmail.com>
2021-04-05 16:37:48 +0200

    more test_clinet,server "SOCKET"

--- Python/python-socket-home/test_server.py
+++ Python/python-socket-home/test_server.py
@@ -1,13 +1,24 @@
 import socket
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import time
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
 
 while True:
     clientsocket, address = s.accept()
-    print(f"connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the Server!!", "utf-8"))
+    print(f"connection from  {address} has been established!")
 
+    msg = "Welcome to the server!"
+    # in the bottom line ":<" means string allignment :D
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    print(msg)
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        clientsocket.send(bytes(msg, "utf-8"))
